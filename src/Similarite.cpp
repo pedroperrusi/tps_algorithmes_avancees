b4691af5bf632a4b9204329a55b545710c999969
SURUAGY-PERRUSI PEDRO-HENRIQUE <suruagyperrusi@etu.unistra.fr>
2019-04-05 11:35:19 +0200

    Small typo correction

--- src/Similarite.cpp
+++ src/Similarite.cpp
@@ -1,20 +1,21 @@
 #include "recalage/Similarite.h"
 #include <math.h>
+
 double SimilariteQuadratique::execute (const Image& im1, const Image& mask1, const Image& im2, const Image& mask2)
 {
 	double e = 0;
-	int compteur=0;
+	int compteur = 0;
 
 	for (int i=0; i<im1.getWidth(); i++){
 		for (int j=0; j<im1.getHeight(); j++){
-			if (mask1.getValue(i,j)==1 and mask2.getValue(i,j)==1)
+			if (mask1.getValue(i,j)==1 && mask2.getValue(i,j)==1)
 				{
 				e = e+pow(im1.getValue(i,j)-im2.getValue(i,j),2);
 				compteur++;
 				}
 			}
 		}
-	e=e/compteur;
+	e = e / compteur;
 
 	return e;
 }
