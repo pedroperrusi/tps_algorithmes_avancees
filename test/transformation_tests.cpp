2ef31f00700021be5d6b96ac35aa23f682b378d8
MENDES FERREIRA Lucas Emilio <mendesferreira@a202pc02.psi.ad.unistra.fr>
2019-04-25 14:35:26 +0200

    Ajout de tests de la classe transformation pour une rotation

--- test/transformation_tests.cpp
+++ test/transformation_tests.cpp
@@ -2,6 +2,7 @@
  * Unittests file for Transformation
  */
 #include "gtest/gtest.h"
+#include <math.h>
 
 #include "recalage/Image.h"
 #include "recalage/Point.h"
@@ -26,4 +27,20 @@ TEST(Transformation, transformation_get_set_param)
 
     ASSERT_TRUE(outPoint.getX() == point_test.getX());
     ASSERT_TRUE(outPoint.getY() == point_test.getY());
+}
+
+TEST(Transformation, transformation_rotation_pi_2){
+	Parametres param_test(3);
+	
+	p_test[0] = 0;
+	p_test[1] = 0;
+	p_test[2] = M_PI_2;
+	
+	TransformationRigide trans_test = Transformation();
+	Point point_test = Point(0,4);
+	
+    Point outPoint = trans_test.execute(param_test, point_test);
+	
+	ASSERT_TRUE(outPoint.getX() == -point_test.getY());
+	ASSERT_TRUE(outPoint.getY() == point_test.getX());
 }
