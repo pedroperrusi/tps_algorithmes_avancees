cb2a4c45a271b6da01cad099182c1b74bc321a01
Lucas Ferreira <lucasemilio.mf@gmail.com>
2019-04-25 14:53:10 +0200

    Ajout de tests de la classe transformation pour une translation

--- test/transformation_tests.cpp
+++ test/transformation_tests.cpp
@@ -44,3 +44,19 @@ TEST(Transformation, transformation_rotation_pi_2){
 	ASSERT_TRUE(outPoint.getX() == -point_test.getY());
 	ASSERT_TRUE(outPoint.getY() == point_test.getX());
 }
+
+TEST(Transformation, transformation_translation){
+	Parametres param_test(3);
+	
+	p_test[0] = 5;
+	p_test[1] = 2;
+	p_test[2] = 0;
+	
+	TransformationRigide trans_test = Transformation();
+	Point point_test = Point(3,5);
+	
+    Point outPoint = trans_test.execute(param_test, point_test);
+	
+	ASSERT_TRUE(outPoint.getX() == point_test.getX() + p_test[0]);
+	ASSERT_TRUE(outPoint.getY() == point_test.getY() + p_test[1]);
+}
